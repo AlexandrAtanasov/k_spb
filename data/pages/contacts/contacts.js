e29b5110fa718566a0c3c932cae288a8ad1b1601
AtanasovAlexandr <mephistosaa@gmail.com>
2021-08-02 23:06:20 +0300

    updated: map

--- data/pages/contacts/contacts.js
+++ data/pages/contacts/contacts.js
@@ -1,23 +1,9 @@
 export const contacts = [
     {
         id: '1',
-        header: 'Крупской, 31',
-        title: 'Медицинский центр Кинезис на Крупской 31',
+        header: 'Мебельная 49/92',
+        title: 'Центр кинезитерапии на улице Мебельной 49/92',
         text: 'Address of center #1 **here**',
-        map: 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Adea7629fc5a568c12339c0e2450a4e623a1d4591b22ac43c66f3198bbf948990&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true',
-    },
-    {
-        id: '2',
-        header: 'Ленина, 87',
-        title: 'Медицинский центр Кинезис на Ленина 87',
-        text: 'Address of center #2 **here**',
-        map: 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Adea7629fc5a568c12339c0e2450a4e623a1d4591b22ac43c66f3198bbf948990&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true',
-    },
-    {
-        id: '3',
-        header: 'Маршала Рыбалко, 85В',
-        title: 'Физкультурно-оздоровительный центр Кинезис на Маршала Рыбалко 85В',
-        text: 'Address of center #3 **here**',
-        map: 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Adea7629fc5a568c12339c0e2450a4e623a1d4591b22ac43c66f3198bbf948990&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true',
+        map: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abc2ea2478e78790d6e4c73d17bde597dd853a35872b3313ac4dc08a25091418b&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>'
     }
 ]
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -34,7 +34,7 @@ export default function LocationsPage() {
     return (
         <MainLayout
             title='Как нас найти'
-            description='Адреса центров Кинезис в Перми'
+            description='Наш адрес в городе Санкт-Петербурге'
         >
             <HeadingComponent 
                 heading='Как нас найти'
@@ -75,35 +75,7 @@ export default function LocationsPage() {
                                             source={contacts[0].text} 
                                         /> */}
                                     </Card.Body>
-                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A95a8d88c04c44184a9ee71ef86a61c6658c142efe7b57e5a0d8b35955ce599c5&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
-                                </Card>
-                            </Tab.Pane>
-                            
-                            <Tab.Pane eventKey={contacts[1].id} key={contacts[1].id}>
-                                <Card id="card_contacts">
-                                    <Card.Body>
-                                        <Card.Title>
-                                            {contacts[1].title}
-                                        </Card.Title>
-                                        {/* <ReactMarkdown
-                                            source={contacts[1].text} 
-                                        /> */}
-                                    </Card.Body>
-                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Adea7629fc5a568c12339c0e2450a4e623a1d4591b22ac43c66f3198bbf948990&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
-                                </Card>
-                            </Tab.Pane>
-
-                            <Tab.Pane eventKey={contacts[2].id} key={contacts[2].id}>
-                                <Card id="card_contacts">
-                                    <Card.Body>
-                                        <Card.Title>
-                                            {contacts[2].title}
-                                        </Card.Title>
-                                        {/* <ReactMarkdown
-                                            source={contacts[2].text} 
-                                        /> */}
-                                    </Card.Body>
-                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A7e2b36bc87df5c5ddcab1036a5def55fa532335f6506744aa4dd575dba36fcc5&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
+                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Abc2ea2478e78790d6e4c73d17bde597dd853a35872b3313ac4dc08a25091418b&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
                                 </Card>
                             </Tab.Pane>
 
