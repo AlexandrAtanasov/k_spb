aecd9407039a09777f3a8950295a0e660bd8fbda
AtanasovAlexandr <mephistosaa@gmail.com>
2022-01-16 23:49:41 +0300

    updated: правки в текстах

--- data/menu/cardiovascular/cardiovascular_menu.js
+++ data/menu/cardiovascular/cardiovascular_menu.js
@@ -1,3 +1,10 @@
 export const cardiovascular_menu = [
-    { id: 1, title:"Реабилитация после инсульта", pid: 'reabilitaciya-posle-insulta' },
+    { id: 1, title:"Гипертония", pid: 'hypertension' },
+    { id: 2, title:"Реабилитация после инсульта", pid: 'reabilitaciya-posle-insulta' },
+    { id: 3, title:"Инфаркт", pid: 'heartattack' },
+    { id: 4, title:"Легочная эмболия", pid: 'pulmonaryembolism' },
+    { id: 5, title:"Атеросклероз", pid: 'atherosclerosis' },
+    { id: 6, title:"Тромбоз", pid: 'thrombosis' },
+    { id: 7, title:"Варикоз", pid: 'varicoseveins' },
+    { id: 8, title:"Ишемия", pid: 'ischemia' },
 ];
--- data/menu/spine/spine_menu.js
+++ data/menu/spine/spine_menu.js
@@ -1,7 +1,9 @@
 export const spine_menu = [
-    { id: 1, title:"Сколиоз", pid: 'scolioz' },
+    { id: 1, title:"Боли в спине", pid: 'boli-v-spine' },
     { id: 2, title:"Остеохондроз", pid: 'osteohondroz' },
-    { id: 3, title:"Боли в спине", pid: 'boli-v-spine' },
-    { id: 4, title:"Нарушение осанки", pid: 'narusheniya-osanki' },
-    { id: 5, title:"Грыжа позвоночника", pid: 'gryzha-pozvonochnika' },
+    { id: 3, title:"Грыжа позвоночника", pid: 'gryzha-pozvonochnika' },
+    { id: 4, title:"Сколиоз", pid: 'scolioz' },
+    { id: 5, title:"Остеопороз", pid: 'osteoporoz' },
+    { id: 6, title:"Спондилолистез", pid: 'spondilolistez' },
+    { id: 7, title:"Болезнь Бехтерева", pid: 'behterev' },
 ];
